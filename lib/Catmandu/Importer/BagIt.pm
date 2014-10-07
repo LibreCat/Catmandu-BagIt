9b3df2bfc9377e6491718e315a996afc0b2919bf
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2014-10-07 16:21:16 +0200

    Adding a bit more body to the importer

--- lib/Catmandu/Importer/BagIt.pm
+++ lib/Catmandu/Importer/BagIt.pm
@@ -28,7 +28,44 @@ sub generator {
 sub read_bag {
 	my ($self,$dir) = @_;
 	my $bag = Archive::BagIt->new($dir);
-	{ _id => $dir };
+
+    my %manifests = ();
+
+    if ($bag->manifest_files) {
+        for my $f ($bag->manifest_files) {
+            open(my $fh , '<' , $f);
+            while(<$fh>) {
+                chomp;
+                my ($sum,$file) = split(/\s+/,$_,2);
+                $manifests{$file} = $sum; 
+            }
+            close($fh);
+        }
+    }
+
+    my %tagmanifests = ();
+
+    if ($bag->tagmanifest_files) {
+        for my $f ($bag->tagmanifest_files) {
+            open(my $fh , '<' , $f);
+            while(<$fh>) {
+                chomp;
+                my ($sum,$file) = split(/\s+/,$_,2);
+                $tagmanifests{$file} = $sum; 
+            }
+            close($fh);
+        }
+    }
+
+	{ 
+        _id               => $dir ,
+        base              => $dir ,
+        version           => $bag->version ,
+        payload_files     => [ map { substr($_,length($dir) + 1) } $bag->payload_files ] ,
+        non_payload_files => [ map { substr($_,length($dir) + 1) } $bag->non_payload_files ] ,
+        manifest          => \%manifests ,
+        tagmanifest       => \%tagmanifests ,
+    };
 }
 
 1;
